421bb876606a786a6ff2a6702b394ece29e0fbaf
= <akepogurajiv317@gmail.com>
2023-02-26 22:28:36 +0530

    sample java

---
+++ src/main/java/comcast/com/commvn/App.java
@@ -0,0 +1,13 @@
+package comcast.com.commvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
